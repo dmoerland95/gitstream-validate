b12db97fe7f91419451b91ac0491483c98744f3c
Dylan Moerland <dylan@headfwd.com>
2023-07-26 11:38:26 +0200

    Add

---
+++ frontend/platform/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+        <p>Lets try some frontend changes</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
